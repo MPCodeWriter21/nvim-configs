4800d6b17269947487807877b43742e9a3552624
CodeWriter21 <CodeWriter21@gmail.com>
2024-01-23 19:54:49 +0330

    Configured nvterm to use zsh.

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -235,5 +235,36 @@ local plugins = {
     --     dependencies = { "nvim-lua/plenary.nvim", --[[ "nvim-telescope/telescope.nvim" ]] },
     --     event = "VeryLazy"
     -- },
+    {
+        "NvChad/nvterm",
+        opts = function()
+            return {
+                terminals = {
+                    shell = "zsh",
+                    list = {},
+                    type_opts = {
+                        float = {
+                            relative = 'editor',
+                            row = 0.3,
+                            col = 0.25,
+                            width = 0.5,
+                            height = 0.4,
+                            border = "single",
+                        },
+                        horizontal = { location = "rightbelow", split_ratio = .3, },
+                        vertical = { location = "rightbelow", split_ratio = .5 },
+                    }
+                },
+                behavior = {
+                    autoclose_on_quit = {
+                        enabled = false,
+                        confirm = true,
+                    },
+                    close_on_exit = true,
+                    auto_insert = true,
+                },
+            }
+        end
+    },
 }
 return plugins
