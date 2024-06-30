c0988064f0aee05fa55e96b4ebec14315d5f3aa7
CodeWriter21 <CodeWriter21@gmail.com>
2024-07-01 01:28:58 +0330

    Update mason ensure_installeds.

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -74,7 +74,9 @@ local plugins = {
                 "jdtls",
                 "yaml-language-server",
                 "prosemd-lsp",
-                "codelldb"
+                "codelldb",
+                "biome",
+                "typescript-language-server"
             },
         },
     },
@@ -280,9 +282,40 @@ local plugins = {
     {
         "folke/trouble.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            vim.keymap.set('n', '<leader>tt', ':TroubleToggle<CR>')
-        end,
+        opts = {}, -- for default options, refer to the configuration section for custom setup.
+        cmd = "Trouble",
+        keys = {
+            {
+                "<leader>xx",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<leader>xX",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>cs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+                "<leader>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
+            },
+            {
+                "<leader>xQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
+            },
+        },
         event = "VeryLazy"
     },
     {
