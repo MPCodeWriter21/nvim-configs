493b3443fe56f1527078f678760e2c3226bd1de3
CodeWriter21 <CodeWriter21@gmail.com>
2024-09-14 13:34:09 +0330

    none-ls: formatting: markdownlint

--- lua/custom/configs/none-ls.lua
+++ lua/custom/configs/none-ls.lua
@@ -46,10 +46,11 @@ local opts = {
     sources = {
         -- null_ls.builtins.diagnostics.mypy,
         -- null_ls.completion.spell,
-        null_ls.builtins.diagnostics.curlylint,
+        -- null_ls.builtins.diagnostics.curlylint,
         null_ls.builtins.diagnostics.djlint,
         null_ls.builtins.diagnostics.erb_lint,
         null_ls.builtins.diagnostics.tidy,
+        null_ls.builtins.formatting.markdownlint,
         -- null_ls.builtins.formatting.htmlbeautifier,
     },
 }
