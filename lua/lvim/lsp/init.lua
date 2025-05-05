bbaa3a96747225034858673d3682df8fe9dfdf2f
CodeWriter21 <CodeWriter21@gmail.com>
2025-05-05 17:47:35 +0330

    fix: Switched to using `vim.lsp.buf.signature_help` instead of deprecated `vim.lsp.handlers.signature_help`

--- lua/lvim/lsp/init.lua
+++ lua/lvim/lsp/init.lua
@@ -128,7 +128,7 @@ function M.setup()
         end
     end
     set_handler_opts_if_not_set("textDocument/hover", vim.lsp.handlers.hover, { border = "rounded" })
-    set_handler_opts_if_not_set("textDocument/signatureHelp", vim.lsp.handlers.signature_help, { border = "rounded" })
+    set_handler_opts_if_not_set("textDocument/signatureHelp", vim.lsp.buf.signature_help, { border = "rounded" })
 
     -- Enable rounded borders in :LspInfo window.
     require("lspconfig.ui.windows").default_options.border = "rounded"
