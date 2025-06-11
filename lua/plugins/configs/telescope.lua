d53e91e08f531041cedc901381a2c9ba67a8515f
CodeWriter21 <CodeWriter21@gmail.com>
2025-06-11 14:47:49 +0330

    fix: Telescope key binds

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -48,9 +48,11 @@ local options = {
         -- Developer configurations: Not meant for general override
         buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
         mappings = {
-            n = { ["q"] = require("telescope.actions").close },
+            n = {
+                ["q"] = require("telescope.actions").close,
+                ["<c-t>"] = open_with_trouble
+            },
             i = { ["<c-t>"] = open_with_trouble },
-            n = { ["<c-t>"] = open_with_trouble },
         },
     },
 
